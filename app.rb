786c536cf8de8b7bf0c73a611fba228f0efaad95
Eden Mesfun <emesfun@gmail.com>
2018-03-23 14:20:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
 
     @puppy = Puppy.new("#{params[:name]}", "#{params[:breed]}", "#{params[:age]}")
 
-    @puppy_info = []
+    @puppy_info = [""]
     erb :display_puppy
   end
 
